d58b9682da990688149ff73c283a17d206f9045f
Natasha Kelly <kellynatashav@yahoo.com>
2017-08-21 20:10:26 -0400

    add model, views, and controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @content = 'This is a sample post for post one.'
+    @content_second = "This is a sample post for post two."
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
